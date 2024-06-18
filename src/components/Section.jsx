ecd1a668476cac96ddb9106619c5d1c6b342647e
Gabriel <wrangleronline@hotmail.com>
2024-06-18 14:18:19 +0200

    NEw TwinkleStar component used in Section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TwinkleStar from "./TwinkleStar.jsx";
 
 export default function Section() {
   return (
@@ -32,50 +33,7 @@ export default function Section() {
           </div>
         </div>
       </div>
-      <span className="absolute left-1/2 top-1/4">
-        <svg
-          width="56px"
-          height="56px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            opacity="0.15"
-            d="M12 3C12 7.97056 16.0294 12 21 12C16.0294 12 12 16.0294 12 21C12 16.0294 7.97056 12 3 12C7.97056 12 12 7.97056 12 3Z"
-            fill="#000000"
-          />
-          <path
-            d="M12 3C12 7.97056 16.0294 12 21 12C16.0294 12 12 16.0294 12 21C12 16.0294 7.97056 12 3 12C7.97056 12 12 7.97056 12 3Z"
-            stroke="#000000"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </span>
-      <span className="absolute right-10 top-7">
-        <svg
-          width="104px"
-          height="104px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            opacity="0.15"
-            d="M12 3C12 7.97056 16.0294 12 21 12C16.0294 12 12 16.0294 12 21C12 16.0294 7.97056 12 3 12C7.97056 12 12 7.97056 12 3Z"
-            fill="#000000"
-          />
-          <path
-            d="M12 3C12 7.97056 16.0294 12 21 12C16.0294 12 12 16.0294 12 21C12 16.0294 7.97056 12 3 12C7.97056 12 12 7.97056 12 3Z"
-            stroke="#000000"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </span>
+      <TwinkleStar/>
     </section>
   );
 }
